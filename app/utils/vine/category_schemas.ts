a1dd321c78b26dcbd76802302a745c9374b63ddd
Timeuh <timeuuh@gmail.com>
2025-10-21 15:29:18 +0200

    🦺 add category CRUD schemas and types

---
+++ app/utils/vine/category_schemas.ts
@@ -0,0 +1,45 @@
+import vine from '@vinejs/vine';
+import {Infer} from '@vinejs/vine/types';
+
+/* -------------------------------------------------------------------------- */
+/*                                  Schemas                                   */
+/* -------------------------------------------------------------------------- */
+const CATEGORY_SCHEMA = vine.object({
+  id: vine.number(),
+  name: vine.string(),
+});
+
+const CATEGORY_CREATE_SCHEMA = vine.object({
+  name: vine.string(),
+});
+
+const CATEGORY_UPDATE_SCHEMA = vine.object({
+  id: vine.number(),
+  name: vine.string(),
+});
+
+const CATEGORY_DELETE_SCHEMA = vine.object({
+  id: vine.number(),
+});
+
+/* -------------------------------------------------------------------------- */
+/*                                   Types                                    */
+/* -------------------------------------------------------------------------- */
+export type Category = Infer<typeof CATEGORY_SCHEMA>;
+
+export type CategoryCreateSchema = Infer<typeof CATEGORY_CREATE_SCHEMA>;
+
+export type CategoryUpdateSchema = Infer<typeof CATEGORY_UPDATE_SCHEMA>;
+
+export type CategoryDeleteSchema = Infer<typeof CATEGORY_DELETE_SCHEMA>;
+
+/* -------------------------------------------------------------------------- */
+/*                                 Validators                                 */
+/* -------------------------------------------------------------------------- */
+export const categoryValidator = vine.compile(CATEGORY_SCHEMA);
+
+export const categoryCreateValidator = vine.compile(CATEGORY_CREATE_SCHEMA);
+
+export const categoryUpdateValidator = vine.compile(CATEGORY_UPDATE_SCHEMA);
+
+export const categoryDeleteValidator = vine.compile(CATEGORY_DELETE_SCHEMA);
