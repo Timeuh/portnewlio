9d0d2f7e1a992c8b313d8c52c3f129a823fd7b39
Timeuh <timeuuh@gmail.com>
2025-10-21 17:21:13 +0200

    ✨ create route to get a category from its id

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -1,19 +1,21 @@
-import {HTTP_OK} from '@utils/constants/api';
+import {HTTP_NOT_FOUND, HTTP_OK, MSG_NOT_FOUND} from '@constants/api';
 import sendErrorResponse from '@functions/api/send_error_response';
 import sendJsonResponse from '@functions/api/send_json_response';
 import {prisma} from '@utils/prisma/client';
 import {Category, CategoryUpdate, categoryUpdateValidator} from '@appVine/category_schemas';
-import {ApiParams} from '@appTypes/api';
+import {ApiError, ApiParams} from '@appTypes/api';
 
 /**
  * Update a category
  *
  * @param request {Request} - The incoming request object
+ * @param apiParams {ApiParams} - The API parameters
  */
 export async function PUT(request: Request, apiParams: ApiParams): Promise<Response> {
   try {
     // read received data
     const body: any = await request.json();
+    const {id} = await apiParams.params;
 
     // validate data
     const validatedData: CategoryUpdate = await categoryUpdateValidator.validate(body);
@@ -21,13 +23,52 @@ export async function PUT(request: Request, apiParams: ApiParams): Promise<Respo
     // update category in database
     const updatedCategory: Category = await prisma.category.update({
       where: {
-        id: parseInt(apiParams.params.id),
+        id: parseInt(id),
       },
       data: validatedData,
     });
 
     // return updated category
     return sendJsonResponse<Category>(updatedCategory, HTTP_OK);
+  } catch (error: unknown) {
+    return sendErrorResponse(error);
+  }
+}
+
+/**
+ * Get a category from id
+ *
+ * @param request {Request} - The incoming request object
+ * @param apiParams {ApiParams} - The API parameters
+ */
+export async function GET(_request: Request, apiParams: ApiParams): Promise<Response> {
+  try {
+    // get data from request
+    const {id} = await apiParams.params;
+
+    // get category from database
+    const category: Category | null = await prisma.category.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    // if the category does not exist, return not found error
+    if (!category) {
+      return sendJsonResponse<ApiError>(
+        {
+          error: {
+            code: HTTP_NOT_FOUND,
+            message: MSG_NOT_FOUND,
+            details: `Category with id ${id} not found`,
+          },
+        },
+        HTTP_NOT_FOUND,
+      );
+    }
+
+    // return category
+    return sendJsonResponse<Category>(category, HTTP_OK);
   } catch (error: unknown) {
     return sendErrorResponse(error);
   }
