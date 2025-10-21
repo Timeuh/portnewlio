76399e164d5bf87f0d75ef3ace4f790b8d59df66
Timeuh <timeuuh@gmail.com>
2025-10-21 16:00:32 +0200

    ✨ create category in database and return it

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -1,9 +1,16 @@
 import {HTTP_CREATED} from '@/app/utils/constants/api';
 import sendErrorResponse from '@/app/utils/functions/api/send_error_response';
 import sendJsonResponse from '@/app/utils/functions/api/send_json_response';
+import {prisma} from '@/app/utils/prisma/client';
 import {CategoryCreation, categoryCreateValidator} from '@/app/utils/vine/category_schemas';
+import {Category} from '@prisma/client';
 
-export async function POST(request: Request) {
+/**
+ * Create a new category
+ *
+ * @param request {Request} - The incoming request object
+ */
+export async function POST(request: Request): Promise<Response> {
   try {
     // read received data
     const body: any = await request.json();
@@ -11,8 +18,13 @@ export async function POST(request: Request) {
     // validate data
     const validatedData: CategoryCreation = await categoryCreateValidator.validate(body);
 
-    // return data to client for testing
-    return sendJsonResponse<CategoryCreation>(validatedData, HTTP_CREATED);
+    // create new category in database
+    const createdCategory: Category = await prisma.category.create({
+      data: validatedData,
+    });
+
+    // return created category
+    return sendJsonResponse<Category>(createdCategory, HTTP_CREATED);
   } catch (error: unknown) {
     return sendErrorResponse(error);
   }
