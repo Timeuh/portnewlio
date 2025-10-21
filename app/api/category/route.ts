a4618bbde13cb220946889414553ec5b0ce0c618
Timeuh <timeuuh@gmail.com>
2025-10-21 15:49:19 +0200

    ♻️ validate data before returning and handle errors

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -1,6 +1,19 @@
+import {HTTP_CREATED} from '@/app/utils/constants/api';
+import sendErrorResponse from '@/app/utils/functions/api/send_error_response';
+import sendJsonResponse from '@/app/utils/functions/api/send_json_response';
+import {CategoryCreateSchema, categoryCreateValidator} from '@/app/utils/vine/category_schemas';
+
 export async function POST(request: Request) {
-  // read received data
-  const body = await request.json();
-  // return data to client for testing
-  return Response.json({message: 'Category received', data: body});
+  try {
+    // read received data
+    const body: any = await request.json();
+
+    // validate data
+    const validatedData: CategoryCreateSchema = await categoryCreateValidator.validate(body);
+
+    // return data to client for testing
+    return sendJsonResponse<CategoryCreateSchema>(validatedData, HTTP_CREATED);
+  } catch (error: unknown) {
+    return sendErrorResponse(error);
+  }
 }
