e3e6d2eeebbbe731d581456d96fa8dc0ff5c2b55
Timeuh <timeuuh@gmail.com>
2025-10-22 15:59:18 +0200

    🦺 add project and technology link schema

--- app/utils/vine/technology_schemas.ts
+++ app/utils/vine/technology_schemas.ts
@@ -33,6 +33,11 @@ const TECHNOLOGY_DELETE_SCHEMA = vine.object({
   id: vine.number(),
 });
 
+const TECHNOLOGY_LINK_PROJECT_SCHEMA = vine.object({
+  technology_id: vine.number(),
+  project_id: vine.number(),
+});
+
 /* -------------------------------------------------------------------------- */
 /*                                   Types                                    */
 /* -------------------------------------------------------------------------- */
@@ -44,6 +49,8 @@ export type TechnologyUpdate = Infer<typeof TECHNOLOGY_UPDATE_SCHEMA>;
 
 export type TechnologyDeletion = Infer<typeof TECHNOLOGY_DELETE_SCHEMA>;
 
+export type TechnologyLinkProject = Infer<typeof TECHNOLOGY_LINK_PROJECT_SCHEMA>;
+
 /* -------------------------------------------------------------------------- */
 /*                                 Validators                                 */
 /* -------------------------------------------------------------------------- */
@@ -54,3 +61,5 @@ export const TechnologyCreateValidator = vine.compile(TECHNOLOGY_CREATE_SCHEMA);
 export const TechnologyUpdateValidator = vine.compile(TECHNOLOGY_UPDATE_SCHEMA);
 
 export const TechnologyDeleteValidator = vine.compile(TECHNOLOGY_DELETE_SCHEMA);
+
+export const TechnologyLinkProjectValidator = vine.compile(TECHNOLOGY_LINK_PROJECT_SCHEMA);
