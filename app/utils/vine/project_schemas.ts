2bfd4131eadfcb17edf5e0d8a6c516dba50d0e16
Timeuh <timeuuh@gmail.com>
2025-10-23 13:12:55 +0200

    🦺 create another schema for project with full content and separate from basic project object

--- app/utils/vine/project_schemas.ts
+++ app/utils/vine/project_schemas.ts
@@ -10,29 +10,33 @@ const PROJECT_SCHEMA = vine.object({
   description: vine.string(),
   github: vine.string(),
   link: vine.string(),
-  Project_Technology: vine
-    .array(
-      vine.object({
-        project_id: vine.number(),
-        technology_id: vine.number(),
-        technology: vine.object({
-          id: vine.number(),
-          name: vine.string(),
-          logo_name: vine.string(),
-          category_id: vine.number(),
-        }),
-      }),
-    )
-    .optional(),
-  Highlight: vine
-    .array(
-      vine.object({
+});
+
+const PROJECT_FULL_SCHEMA = vine.object({
+  id: vine.number(),
+  title: vine.string(),
+  description: vine.string(),
+  github: vine.string(),
+  link: vine.string(),
+  Project_Technology: vine.array(
+    vine.object({
+      project_id: vine.number(),
+      technology_id: vine.number(),
+      technology: vine.object({
         id: vine.number(),
-        text: vine.string(),
-        project_id: vine.number(),
+        name: vine.string(),
+        logo_name: vine.string(),
+        category_id: vine.number(),
       }),
-    )
-    .optional(),
+    }),
+  ),
+  Highlight: vine.array(
+    vine.object({
+      id: vine.number(),
+      text: vine.string(),
+      project_id: vine.number(),
+    }),
+  ),
 });
 
 const PROJECT_CREATE_SCHEMA = vine.object({
@@ -58,6 +62,8 @@ const PROJECT_DELETE_SCHEMA = vine.object({
 /* -------------------------------------------------------------------------- */
 export type Project = Infer<typeof PROJECT_SCHEMA>;
 
+export type ProjectFull = Infer<typeof PROJECT_FULL_SCHEMA>;
+
 export type ProjectCreation = Infer<typeof PROJECT_CREATE_SCHEMA>;
 
 export type ProjectUpdate = Infer<typeof PROJECT_UPDATE_SCHEMA>;
@@ -68,6 +74,8 @@ export type ProjectDeletion = Infer<typeof PROJECT_DELETE_SCHEMA>;
 /*                                 Validators                                 */
 /* -------------------------------------------------------------------------- */
 export const ProjectValidator = vine.compile(PROJECT_SCHEMA);
+
+export const ProjectFullValidator = vine.compile(PROJECT_FULL_SCHEMA);
 
 export const ProjectCreateValidator = vine.compile(PROJECT_CREATE_SCHEMA);
 
