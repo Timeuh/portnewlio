efdb4aa448596a46f3b9f6129ac659871d6f305a
Timeuh <timeuuh@gmail.com>
2025-10-24 15:43:52 +0200

    🚧 create skills section of home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import LandingView from '@/app/views/LandingView';
+import SkillsView from '@/app/views/SkillsView';
 
 export default function Home() {
   return (
     <main className='bg-dark font-bungee h-full w-full text-white'>
       <LandingView />
+      <SkillsView />
     </main>
   );
 }
---
+++ app/views/SkillsView.tsx
@@ -0,0 +1,10 @@
+/**
+ * Home page skills view
+ */
+export default function SkillsView() {
+  return (
+    <section id='home-skills' className='flex h-screen w-full flex-col items-center justify-center'>
+      <h2 className='text-center text-3xl'>Mes compétences</h2>
+    </section>
+  );
+}
