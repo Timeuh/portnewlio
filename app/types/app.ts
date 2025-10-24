a28c08d66e58311aa3686ae750a5798e53ca1a26
Timeuh <timeuuh@gmail.com>
2025-10-24 18:12:59 +0200

    🏷️ create type ordered skills for skills display

--- app/types/app.ts
+++ app/types/app.ts
@@ -1,6 +1,15 @@
+import {TechnologyFull} from '@appVine/technology_schemas';
+
 // link in the navigation bar
 export type NavLink = {
   href: string;
   label: string;
   bgColor: string;
+};
+
+// ordered skills for display
+export type OrderedSkills = {
+  frontend: TechnologyFull[];
+  backend: TechnologyFull[];
+  tools: TechnologyFull[];
 };
