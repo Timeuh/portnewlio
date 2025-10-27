3ae0ce11c7bd920202675a575e78e4b5dd276529
Timeuh <timeuuh@gmail.com>
2025-10-27 15:47:37 +0100

    ✨ display hobbies view

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import HobbiesView from '@/app/views/HobbiesView';
 import LandingView from '@/app/views/LandingView';
 import SkillsView from '@/app/views/SkillsView';
 
@@ -6,6 +7,7 @@ export default function Home() {
     <main className='bg-dark font-bungee h-full w-full text-white'>
       <LandingView />
       <SkillsView />
+      <HobbiesView />
     </main>
   );
 }
