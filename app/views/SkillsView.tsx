be0d66531159622fdc302530f52421cc8eb56334
Timeuh <timeuuh@gmail.com>
2025-10-27 14:57:26 +0100

    💄 adjust spacing

--- app/views/SkillsView.tsx
+++ app/views/SkillsView.tsx
@@ -18,8 +18,11 @@ export default async function SkillsView() {
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <section id='home-skills' className='flex h-fit w-full flex-col items-center justify-center'>
-        <h2 className='text-center text-3xl'>Mes compétences</h2>
+      <section
+        id='home-skills'
+        className='flex h-fit min-h-screen w-full flex-col items-center justify-center space-y-12 overflow-hidden py-6 xl:space-y-[10vh] xl:py-[10vh]'
+      >
+        <h2 className='text-center text-3xl xl:pb-6 xl:text-5xl'>Mes compétences</h2>
         <SkillsSection />
       </section>
     </HydrationBoundary>
