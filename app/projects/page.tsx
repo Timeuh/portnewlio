d4c961dda4513d7e7fab57d6973d6f0bf510ea0f
Timeuh <timeuuh@gmail.com>
2025-10-29 12:01:50 +0100

    🚧 create projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * Projects page
+ */
+export default function Projects() {
+  return (
+    <main className='bg-dark font-bungee h-full w-full text-white'>
+      <h1>Projects page</h1>
+    </main>
+  );
+}
