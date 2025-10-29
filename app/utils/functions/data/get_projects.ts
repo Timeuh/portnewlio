ac1592e8183c1ab84d9fd3c04bb3f9e436c817d6
Timeuh <timeuuh@gmail.com>
2025-10-29 15:44:33 +0100

    ✨ create function to fetch projects from the api

---
+++ app/utils/functions/data/get_projects.ts
@@ -0,0 +1,29 @@
+import {ApiCollection} from '@/app/types/api';
+import {ProjectFull} from '@appVine/project_schemas';
+
+/**
+ * Fetch projects data from the API
+ */
+export const getProjects = async (): Promise<ApiCollection<ProjectFull>> => {
+  // instanciate empty projects collection
+  const emptyProjects: ApiCollection<ProjectFull> = {
+    type: 'collection',
+    count: 0,
+    items: [],
+  };
+
+  //fetch data from the api
+  const response = await fetch('/api/projects?fullContent=true');
+
+  // if the request failed, return empty projects
+  if (!response.ok) {
+    console.error('❌ API error:', response.status, response.statusText);
+    return emptyProjects;
+  }
+
+  // read data
+  const data: ApiCollection<ProjectFull> = await response.json();
+
+  // return the data
+  return data;
+};
