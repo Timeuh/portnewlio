764a9a1b7cbee5dda958f930a17ee86146bd7627
Timeuh <timeuuh@gmail.com>
2025-10-29 15:45:20 +0100

    ✨ use projects view

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,12 @@
+import ProjectsView from './views/ProjectsView';
+
 /**
  * Projects page
  */
 export default function Projects() {
   return (
     <main className='bg-dark font-bungee h-full w-full text-white'>
-      <h1>Projects page</h1>
+      <ProjectsView />
     </main>
   );
 }
