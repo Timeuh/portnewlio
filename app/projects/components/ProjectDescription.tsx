c1f8bb4f6e6461d10d81b75dd4ae86f2e4b08091
Timeuh <timeuuh@gmail.com>
2025-10-29 16:48:39 +0100

    💄 adjust icons sizes and text styles

--- app/projects/components/ProjectDescription.tsx
+++ app/projects/components/ProjectDescription.tsx
@@ -20,22 +20,22 @@ type Props = {
 export default function ProjectDescription({project, displayColor}: Props) {
   return (
     <>
-      <div className='flex flex-row items-center space-x-4 self-start'>
-        <SparklesIcon className={`${projectColors[displayColor].text} size-10`} />
-        <h2 className='text-2xl font-bold'>{project.title}</h2>
+      <div className='flex flex-row items-center space-x-2 self-start'>
+        <SparklesIcon className={`${projectColors[displayColor].text} size-8`} />
+        <h2 className='text-3xl font-bold'>{project.title}</h2>
       </div>
-      <p>{project.description}</p>
+      <p className='text-xl'>{project.description}</p>
       <div className='flex flex-col space-y-4 self-start'>
         {project.Highlight.map((highlight: HighLight) => (
           <div key={highlight.id} className='flex flex-row items-center space-x-2'>
-            <PlusIcon className={`${projectColors[displayColor].text} size-4`} />
-            <h3>{highlight.text}</h3>
+            <PlusIcon className={`${projectColors[displayColor].text} size-6`} />
+            <h3 className='text-creme'>{highlight.text}</h3>
           </div>
         ))}
       </div>
       <div className='flex flex-row flex-wrap items-center gap-4'>
         {project.Project_Technology.map((projectTech: ProjectTechnology) => (
-          <Technology key={projectTech.technology.id} technology={projectTech.technology} />
+          <Technology key={projectTech.technology.id} technology={projectTech.technology} purpose='projects' />
         ))}
       </div>
     </>
