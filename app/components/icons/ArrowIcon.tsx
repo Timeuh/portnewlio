e7e6a1040bbab32c378eb3e3b63edbb9e3d8d132
Timeuh <timeuuh@gmail.com>
2025-10-29 13:54:48 +0100

    ✨ add arrow icon for carousel component

---
+++ app/components/icons/ArrowIcon.tsx
@@ -0,0 +1,27 @@
+type Props = {
+  className?: string;
+};
+
+/**
+ * Lucide Arrow icon component
+ *
+ * @param className {string} - Optional CSS class names
+ */
+export default function ArrowIcon({className}: Props) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}
+    >
+      <path d='M13.73 4a2 2 0 0 0-3.46 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z' />
+    </svg>
+  );
+}
