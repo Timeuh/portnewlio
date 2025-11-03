5b8c98a2d1836a05eb4a6e57303bb91129c21aff
Timeuh <timeuuh@gmail.com>
2025-11-03 13:03:11 +0100

    ✨ create function to fetch experiences from the api

---
+++ app/utils/functions/data/get_experiences.ts
@@ -0,0 +1,29 @@
+import {ApiCollection} from '@/app/types/api';
+import {ExperienceFull} from '@appVine/experience_schemas';
+
+/**
+ * Fetch experiences data from the API
+ */
+export const getExperiences = async (): Promise<ApiCollection<ExperienceFull>> => {
+  // instanciate empty experiences collection
+  const emptyExperiences: ApiCollection<ExperienceFull> = {
+    type: 'collection',
+    count: 0,
+    items: [],
+  };
+
+  //fetch data from the api
+  const response = await fetch('/api/experiences?fullContent=true');
+
+  // if the request failed, return empty experiences
+  if (!response.ok) {
+    console.error('❌ API error:', response.status, response.statusText);
+    return emptyExperiences;
+  }
+
+  // read data
+  const data: ApiCollection<ExperienceFull> = await response.json();
+
+  // return the data
+  return data;
+};
